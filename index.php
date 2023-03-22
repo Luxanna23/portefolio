aec497839038f3666c035c590ebc9388ffb3ff55
Yasmine Amaddah <121093422+Yasmine-Amaddah@users.noreply.github.com>
2023-03-22 14:48:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Portfolio</title>
+</head>
+<body>
+    <header>
+        <div class="head">
+            <div>
+                <h1>Portfolio</h1>
+                <div class="lien" >
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/fansite/">Projet Fansite</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/voyages/">Projet Voyage</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/module-connexion/">Module de connexion</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/livre-or/">Livre d'or</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/reservation-salles/">Reservation salles</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/projet-revision/memory.php">Memory</a></p>
+                    <p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/mycms/">My Cms</a></p>
+					<p class="site"><a href="https://yasmine-amaddah.students-laplateforme.io/projet-revision/connexion.php">Projet Revision</a></p>
+                </div>
+            </div>
+            <img class="img" src="https://images.pexels.com/videos/6800638/aesthetic-book-leisure-picnic-6800638.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" >
+        </div>
+    </header>
+    <footer>
+        <div class="foot">
+            <p>My media: </br>
+                <a class="p" href="https://github.com/Yasmine-Amaddah">My GitHub</a>
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
